6aa4f22f10d3f84f7254344a9550c897f0f4c85b
Валерия Самсонова <samsonova147@hotmail.com>
2018-04-16 23:57:33 +0500

    16 апреля 23:57

--- CW_start/CW_start/CW_start/correlation.cpp
+++ CW_start/CW_start/CW_start/correlation.cpp
@@ -1,50 +1,9 @@
 #include "correlation.h"
+#include "control.h"
 #include <iostream>
 #include <math.h>
 #include <time.h>
 using namespace std;
-
-long long gcd(long long a, long long b) 
-{
-	if (b == 0)
-		return a;
-	return gcd(b, a%b);
-}
-long long mul(long long a, long long b, long long m) 
-{
-	if (b == 1)
-		return a;
-	if (b % 2 == 0) {
-		long long t = mul(a, b / 2, m);
-		return (2 * t) % m;
-	}
-	return (mul(a, b - 1, m) + a) % m;
-}
-
-long long pows(long long a, long long b, long long m) 
-{
-	if (b == 0)
-		return 1;
-	if (b % 2 == 0) {
-		long long t = pows(a, b / 2, m);
-		return mul(t, t, m) % m;
-	}
-	return (mul(pows(a, b - 1, m), a, m)) % m;
-}
-
-bool ferma(long long x) {
-	if (x == 2)
-		return true;
-	srand(time(NULL));
-	for (int i = 0; i<100; i++) {
-		long long a = (rand() % (x - 2)) + 2;
-		if (gcd(a, x) != 1)
-			return false;
-		if (pows(a, x - 1, x) != 1)
-			return false;
-	}
-	return true;
-}
 
 Correlation::Correlation()
 {
@@ -55,12 +14,20 @@ void Correlation::Input()
 {
 	cout << "Input prime" << endl;
 	cin >> prime;
-	if (ferma(prime))
+	if (FermaAlg(prime))
 	{
 		cout << "Input base" << endl;
 		cin >> a_base;
-		cout << "Input value" << endl;
-		cin >> b_value;
+		if (Generator(a_base, prime))
+		{
+			cout << "Input value" << endl;
+			cin >> b_value;
+		}
+		else
+		{
+			cout << "Number is not generator" << endl;
+			Input();
+		}
 	}
 	else
 	{
--- CW_start/CW_start/CW_start/main.cpp
+++ CW_start/CW_start/CW_start/main.cpp
@@ -1,106 +1,17 @@
+#include "correlation.h"
 #include <iostream>
 #include <math.h>
 #include <time.h>
 using namespace std;
 
-#include "correlation.h"
 
-/*long long gcd(long long a, long long b) {
-	if (b == 0)
-		return a;
-	return gcd(b, a%b);
-}
-long long mul(long long a, long long b, long long m) {
-	if (b == 1)
-		return a;
-	if (b % 2 == 0) {
-		long long t = mul(a, b / 2, m);
-		return (2 * t) % m;
-	}
-	return (mul(a, b - 1, m) + a) % m;
-}
-
-long long pows(long long a, long long b, long long m) {
-	if (b == 0)
-		return 1;
-	if (b % 2 == 0) {
-		long long t = pows(a, b / 2, m);
-		return mul(t, t, m) % m;
-	}
-	return (mul(pows(a, b - 1, m), a, m)) % m;
-}
-
-bool ferma(long long x) {
-	if (x == 2)
-		return true;
-	srand(time(NULL));
-	for (int i = 0; i<100; i++) {
-		long long a = (rand() % (x - 2)) + 2;
-		if (gcd(a, x) != 1)
-			return false;
-		if (pows(a, x - 1, x) != 1)
-			return false;
-	}
-	return true;
-}*/
 
 int main()
 {
-/*	// 1 step
-	long long H, p;
-	cin >> p;
-	if (ferma(p)) {
-		H = trunc(sqrt(p)) + 1;
-		cout << H;
-		// 2 step
-		long long c, a, b;
-		cin >> a;
-		cin >> b;
-		c = long long(pow(a, H)) % p;
-		cout << c << endl;
-		// 3 è 4 step
-		long long *mas1 = new long long[H - 1];
-		long long *mas2 = new long long[H];
-		for (int i = 0; i < H; ++i)
-		{
-			mas1[i] = static_cast<long long>(pow(c, i + 1)) % p;
-			cout << mas1[i] << "\t";
-		}
-		cout << endl;
-		for (int i = 0; i <= H; ++i)
-		{
-			mas2[i] = long long(b*pow(a, i)) % p;
-			cout << mas2[i] << "\t";
-		}
-		cout << endl;
-		// 5 step
-		int u, v;
-		bool flag = true;
-		for (int i = 0; i < H && flag; ++i)
-		{
-			for (int j = 0; j <= H; ++j)
-				if (mas1[i] == mas2[j])
-				{
-					u = i + 1;
-					v = j;
-					flag = false;
-					break;
-				}
-		}
-		cout << u << endl;
-		cout << v << endl;
-		// 6 step
-		long long x;
-		x = long long((H*u - v)) % (p - 1);
-		cout << x << endl;
-	}*/
-
 	Correlation Alg;
 	Alg.CorrAlgorithm();
 	Alg.Output();
 
-
-	getchar();
-	getchar();
+	system("pause");
 	return 0;
 }
